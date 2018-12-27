588e5e9294387ef99175f6bf46a29ae0f648789e
dhlsp <2045039125@qq.com>
2018-12-27 16:38:37 +0800

    修改login

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@views/login/index';
+// import HelloWorld from '@views/login/index';
 
 Vue.use(Router);
+
+const login = (r) => require.ensure([], () => r(require('@views/login')), 'login');
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'index',
+      redirect: 'login',
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login,
     },
   ],
 });
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,32 @@
 <template>
-  <el-row class="login">
-    <div class="login-head-bg"></div>
-    <el-form :model="form" :rules="formRules" ref="form" label-width="0px" class="login-form">
+  <div class="login-container">
+    <el-form class="login-form" ref="form" label-position="left">
+      <div style="font-size: 0px; text-align: center;">
+        <img src="../../assets/imgs/login_test.png" alt="logo" width="250" height="80">
+      </div>
+      <div class="title-container">
+        <h3 class="title">风信子网站</h3>
+      </div>
       <el-form-item prop="account">
-        <el-input v-model="form.account" auto-complete="off" placeholder="账号"></el-input>
+        <span class="svg-container svg-container_login">
+          <svg-icon icon-class="user"/>
+        </span>
+        <el-input autofocus name="account" type="text" v-model.trim="form.account" autoComplete="on" placeholder="账号" />
       </el-form-item>
       <el-form-item prop="password">
-        <el-input type="password" v-model="form.password" auto-complete="off" placeholder="密码"></el-input>
+        <span class="svg-container">
+          <svg-icon icon-class="password"/>
+        </span>
+        <el-input name="password" :type="passwordType" v-model.trim="form.password" autoComplete="on" placeholder="密码" />
+        <span class="show-pwd" @click="showPwd">
+          <svg-icon v-if="passwordType === 'password'" icon-class="noseeeye"/>
+          <svg-icon v-else icon-class="seeeye"/>
+        </span>
       </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="submit" style="width: 100%;">提交</el-button>
-      </el-form-item>
+      <el-button :disabled="disableLogin" type="primary" class="login-btn" :loading="loading" @click="submit">登录</el-button>
     </el-form>
-  </el-row>
+  </div>
+  <!-- <i v-else class="verify-img el-icon-loading"></i> -->
 </template>
 
 <script>
@@ -24,10 +38,16 @@ export default {
         account: '',
         password: '',
       },
+      passwordType: 'password',
       formRules: {},
+      loading: false,
+      disableLogin: false,
     };
   },
   methods: {
+    showPwd() {
+      this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+    },
     submit() {
       this.$router.push({
         path: '/account-list',
@@ -37,24 +57,145 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.login {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 450px;
-  background: #FFFFFF;
-  border-radius: 5px;
+<style rel="stylesheet/scss" lang="scss">
+$bg:#1773a9;
+$light_gray:#eee;
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px !important;
+    width: 85%;
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      &:-webkit-autofill {
+        -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: #fff !important;
+      }
+    }
+  }
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+  .el-form-item__content {
+    height: auto !important;
+  }
+  div.el-form-item {
+    margin-bottom: 17px !important;
+  }
 }
-.login-head-bg {
-  height: 130px;
-  background: no-repeat url(../../assets/imgs/login_test.png);
-  background-size: 100% 100%;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
+</style>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+// $bg:#fff;
+$bg: url(../../assets/imgs/bg.jpg) no-repeat center center;
+$dark_gray:#889aa4;
+$light_gray:#fff;
+.login-container {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background: $bg;
+  background-size: cover; // 让图片全屏覆盖
+  .login-form {
+    position: absolute;
+    left: 80%;
+    top: 50%;
+    width: 450px;
+    // padding: 35px 35px 15px 35px;
+    padding: 10px;
+    transform: translate(-50%, -50%);
+    background: rgba(0,0,0,.5);
+    // border: 1px solid #eaeaea;
+    // box-shadow: 0 0 25px #cac6c6;
+  }
+  .tips {
+    font-size: 14px;
+    color: #fff;
+    margin-bottom: 10px;
+    span {
+      &:first-of-type {
+        margin-right: 16px;
+      }
+    }
+  }
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle; // 属性设置元素的垂直对齐方式
+    width: 30px;
+    display: inline-block;
+    &_login {
+      font-size: 20px;
+    }
+  }
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 26px;
+      font-weight: 400;
+      color: $light_gray;
+      margin: 20px auto 20px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+    .set-language {
+      color: #fff;
+      position: absolute;
+      top: 5px;
+      right: 0px;
+    }
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
+  }
+  .thirdparty-button {
+    position: absolute;
+    right: 35px;
+    bottom: 28px;
+  }
+  .login-btn {
+    width: 100%;
+    margin-bottom: 30px;
+  }
 }
-.login-form {
-  padding: 10px 20px;
-}
+
+// .verify-ipt {
+//   width: 50%;
+//   margin-right: 5%;
+//   display:inline-block;
+// }
+
+// .verify-img {
+//   position: absolute;
+//   top: 0;
+//   right: 4px;
+//   width: 35%;
+//   height: 44px;
+//   margin: 4px 0;
+//   border-radius: 2px;
+//   cursor: pointer;
+// }
+// .verify-img-loading {
+//   background-color: #FFFFFF;
+//   color: #1ABC9C;
+//   line-height: 40px;
+//   font-size: 20px;
+//   text-align: center;
+// }
 </style>
