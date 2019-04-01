2469efb94cc05355fb0a02d287f185706ea7ccef
dls <2045039125@qq.com>
2019-04-01 14:28:57 +0800

    添加列表颜色、表头提示

--- src/views/TableFour/index.vue
+++ src/views/TableFour/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <el-table :data="tableData6" border="" min-width="200" header-align="center" align="center" :summary-method="getSummaries" show-summary style="width: 100%; margin-top: 20px">
-    <el-table-column prop="id" label="ID" width="180" header-align="center" align="center"></el-table-column>
+  <el-table :data="tableData6" border="" min-width="200" header-align="center" align="center" :summary-method="getSummaries" show-summary style="width: 100%; margin-top: 20px" :row-class-name="tableRowClassName">
+    <el-table-column prop="id" label="ID" width="180" header-align="center" align="center" :render-header="renderHeader"></el-table-column>
     <el-table-column prop="name" label="姓名" min-width="100" header-align="center" align="center"></el-table-column>
     <el-table-column prop="amount1" label="数值 1（元）" min-width="100" header-align="center" align="center"></el-table-column>
     <el-table-column prop="amount2" label="数值 2（元）" min-width="100" header-align="center" align="center"></el-table-column>
@@ -21,7 +21,7 @@ export default {
         amount3: '10',
       }, {
         id: '12987123',
-        name: '王小虎',
+        name: '王小未',
         amount1: '165',
         amount2: '4.43',
         amount3: 12,
@@ -39,7 +39,7 @@ export default {
         amount3: 17,
       }, {
         id: '12987126',
-        name: '王小虎',
+        name: '王小二',
         amount1: '539',
         amount2: '4.1',
         amount3: 15,
@@ -96,6 +96,81 @@ export default {
     //   });
     //   return sums;
     // },
+    // 提示
+    // renderHeader(h) {
+    //   return h('span', {
+    //     style: {
+    //       height: '23px',
+    //     },
+    //   }, [
+    //     this.$t('ID'),
+    //     h('el-tooltip', {
+    //       props: {
+    //         effect: 'dark',
+    //         // content: 'Top Center 提示文字',
+    //         placement: 'top',
+    //       },
+    //     }, [
+    //       h('span', {
+    //         slot: 'default',
+    //         style: {
+    //           display: 'inline-block',
+    //           padding: '0 5px',
+    //           color: '#ff0000',
+    //           height: '23px',
+    //           'font-size': '10px',
+    //           'font-weight': 'bold',
+    //         },
+    //       }, '?'),
+    //       h('span', {
+    //         slot: 'content',
+    //       }, '账号的ID号'),
+    //     ]),
+    //   ]);
+    // },
+    renderHeader(h) {
+      return h('span', {}, [
+        h('span', {}, 'ID'),
+        h('el-popover', {
+          props: {
+            placement: 'top-start',
+            // width: '100',
+            trigger: 'hover',
+            // content: '领先/落后品类=单店平均单量-该品类城市店均单量',
+            content: '账号的ID号',
+          },
+        }, [
+          h('i', {
+            slot: 'reference',
+            class: 'el-icon-question',
+            style: {
+              color: '#F9DA28',
+            },
+          }, ''),
+        ]),
+      ]);
+    },
+    // 列表背景颜色
+    tableRowClassName({ row }) {
+      if (row.name === '王小二') {
+        return 'error';
+      }
+      if (row.name === '王小未') {
+        return 'success';
+      }
+    },
   },
 };
 </script>
+
+<style>
+.el-table .warning {
+  background: #ffffe6 !important;
+}
+.el-table .success {
+  background: #eaffeb !important;
+}
+.el-table .error {
+  background: #fdf2f2 !important;
+}
+</style>
