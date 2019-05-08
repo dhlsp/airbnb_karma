6d7c29e201adacc3bb3f3621ff7b0a8ff43831e9
dls <2045039125@qq.com>
2019-05-08 16:00:35 +0800

    添加定时策略打印

--- src/views/FormCron/index.vue
+++ src/views/FormCron/index.vue
@@ -5,6 +5,9 @@
       <cron @change="changeCron" @close="cronPopover=false"></cron>
       <el-input slot="reference" @click="cronPopover=true" v-model="cron" placeholder="请输入定时策略"></el-input>
     </el-popover>
+    <div class="text-center" style="padding-top: 10px;">
+      <el-button :loading="loading" size="medium" type="primary" @click="submit">确定</el-button>
+    </div>
   </div>
 </template>
 
@@ -17,11 +20,21 @@ export default {
     return {
       cronPopover: false,
       cron: '',
+      loading: false,
     };
   },
   methods: {
     changeCron(val) {
       this.cron = val;
+    },
+    submit() {
+      if (this.cron === '') {
+        this.$message.error('选择定时策略');
+        return;
+      }
+      this.loading = true;
+      console.log('this.cron', this.cron);
+      this.loading = false;
     },
   },
   components: {
