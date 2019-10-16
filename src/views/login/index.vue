cf76b25982b8e117ab84e17c8015e13e63dc82c3
dhlsp <dhlsp2018@162.com>
2019-10-16 11:02:23 +0800

    添加验证码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,16 +4,22 @@
       <div class="ms-title">后台管理系统</div>
       <el-form :model="form" :rules="rules" ref="form" label-width="0px" class="ms-content">
         <el-form-item prop="username">
-          <el-input v-model="form.username" placeholder="username">
+          <el-input v-model="form.username" placeholder="请输入用户名">
             <!-- <el-button slot="prepend" icon="el-icon-lx-lock"></el-button> -->
             <svg-icon slot="prepend" icon-class="user"/>
           </el-input>
         </el-form-item>
         <el-form-item prop="password">
-          <el-input type="password" placeholder="password" v-model="form.password" @keyup.enter.native="submit('form')">
+          <el-input type="password" placeholder="请输入密码" v-model="form.password">
             <!-- <el-button slot="prepend" icon="el-icon-lx-lock"></el-button> -->
             <svg-icon slot="prepend" icon-class="password"/>
           </el-input>
+        </el-form-item>
+        <el-form-item prop="verifynum">
+          <el-input type="verifynum" placeholder="请输入验证码" v-model="form.verifynum" @keyup.enter.native="submit('form')" class="verify-ipt">
+            <svg-icon slot="prepend" icon-class="verify"/>
+          </el-input>
+          <div class="verify-img" @click="createCode(4)">{{verifyImg}}</div>
         </el-form-item>
         <div class="login-btn">
           <el-button type="primary" @click="submit('form')">登录</el-button>
@@ -57,7 +63,11 @@ export default {
       form: {
         username: '',
         password: '',
+        verifynum: '',
       },
+      verifyImg: '', // 验证码base64
+      isShowVerifyImg: false, // 是否显示二维码
+      verifyTimer: null, // 验证码定时器
       passwordType: 'password',
       formRules: {},
       loading: false,
@@ -69,16 +79,72 @@ export default {
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
         ],
+        verifynum: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+        ],
       },
     };
+  },
+  created() {
+    this.createCode(4);
+  },
+  beforeDestroy() {
+    window.clearTimeout(this.verifyTimer);
+    this.verifyTimer = null;
   },
   methods: {
     showPwd() {
       this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
     },
+    // 生成验证码方法
+    createCode(len) {
+      if (this.verifyTimer === null) {
+        let code = '';
+        // 验证码长度
+        let codeLength = parseInt(len, 10);
+        // 所有候选组成验证码的字符，当然也可以用中文的
+        let codeChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+        // 循环组成验证码的字符串
+        for (let i = 0; i < codeLength; i++) {
+          // 获取随机验证码下标
+          let charNum = Math.floor(Math.random() * 62);
+          // 组成指定的字符验证码
+          code += codeChars[charNum];
+        }
+        this.verifyImg = code;
+        this.verifyTimer = window.setTimeout(() => {
+          window.clearTimeout(this.verifyTimer);
+          this.verifyTimer = null;
+        }, 1000);
+      }
+      // let code = '';
+      // // 验证码长度
+      // let codeLength = parseInt(len, 10);
+      // // 所有候选组成验证码的字符，当然也可以用中文的
+      // let codeChars = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+      //   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+      //   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+      // // 循环组成验证码的字符串
+      // for (let i = 0; i < codeLength; i++) {
+      //   // 获取随机验证码下标
+      //   let charNum = Math.floor(Math.random() * 62);
+      //   // 组成指定的字符验证码
+      //   code += codeChars[charNum];
+      // }
+      // this.verifyImg = code;
+    },
     submit(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
+          // 检查验证码是否正确
+          if (this.form.verifynum !== this.verifyImg) {
+            this.$message.error('验证码错误');
+            this.createCode(4);
+            this.form.verifynum = '';
+            return;
+          }
           localStorage.setItem('ms_username', this.form.username);
           this.$router.push('/home-page');
         } else {
@@ -97,7 +163,9 @@ export default {
   width:100%;
   height:100%;
   background-image: url(../../assets/imgs/bg.jpg);
-  background-size: 100%;
+  /* background-size: 100%; */
+  background-repeat:no-repeat;
+  background-size: cover;
 }
 .ms-title{
   width:100%;
@@ -133,6 +201,29 @@ export default {
   font-size:12px;
   line-height:30px;
   color:#000;
+}
+.verify-img {
+  font-family:Arial;
+  font-style:italic;
+  color:blue;
+  font-size:30px;
+  border:0;
+  padding:2px 3px;
+  letter-spacing:3px;
+  font-weight:bolder;
+  float:left;
+  cursor:pointer;
+  width: 30%;
+  height: 35px;
+  line-height:35px;
+  text-align:center;
+  vertical-align:middle;
+  background-color:#D8B7E3;
+}
+.verify-ipt {
+  width: 65%;
+  float: left;
+  margin-right: 10px;
 }
 </style>
 
